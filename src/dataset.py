015a09e0e52de35313fd1d6663c37eac7726be2f
As17-01 <aseliverstov@nes.ru>
2025-02-02 23:03:39 +0300

    Updates

--- src/dataset.py
+++ src/dataset.py
@@ -18,6 +18,7 @@ class EEG:
 
     def load_participant(self, participant_id: int):
         self.int = {}
+        self.raw = {}
         self.delta = {}
         self.theta = {}
         self.alpha = {}
@@ -27,14 +28,16 @@ class EEG:
         for i in self.intencities:
             file_name = f"Backgr_int_{i}.dat" if self.image_name == "Figs for spectra" else f"Backgr_int_{i}_type_0.4.dat"
             self.int[i] = np.loadtxt(self.data_dir / f"Participant {participant_id}" / self.image_name / file_name)
+            raw_signal = np.empty(self.int[i].shape)
             delta_signal = np.empty(self.int[i].shape)
             theta_signal =  np.empty(self.int[i].shape)
             alpha_signal = np.empty(self.int[i].shape)
             beta_signal = np.empty(self.int[i].shape)
             gamma_signal = np.empty(self.int[i].shape)
             for c in range(self.int[i].shape[1]):
-                delta_signal[:, c], theta_signal[:, c], alpha_signal[:, c], beta_signal[:, c], gamma_signal[:, c] = self._fir_filtering(i, c)
+                raw_signal[:, c], delta_signal[:, c], theta_signal[:, c], alpha_signal[:, c], beta_signal[:, c], gamma_signal[:, c] = self._fir_filtering(i, c)
 
+            self.raw[i] = raw_signal
             self.delta[i] = delta_signal
             self.theta[i] = theta_signal
             self.alpha[i] = alpha_signal
@@ -42,6 +45,7 @@ class EEG:
             self.gamma[i] = gamma_signal
 
         participant_info = {}
+        participant_info["raw"] = self.raw
         participant_info["delta"] = self.delta
         participant_info["theta"] = self.theta
         participant_info["alpha"] = self.alpha
@@ -57,13 +61,14 @@ class EEG:
         filter_beta = signal.firwin(400, [13.0, 30.0], pass_zero=False, fs=250)
         filter_gamma = signal.firwin(400, [31.0, 45.0], pass_zero=False, fs=250)
 
+        res_raw = self.int[i][:, c]
         res_delta = signal.convolve(self.int[i][:, c], filter_delta, mode='same')
         res_theta = signal.convolve(self.int[i][:, c], filter_theta, mode='same')
         res_alpha = signal.convolve(self.int[i][:, c], filter_alpha, mode='same')
         res_beta = signal.convolve(self.int[i][:, c], filter_beta, mode='same')
         res_gamma = signal.convolve(self.int[i][:, c], filter_gamma, mode='same')
 
-        return res_delta, res_theta, res_alpha, res_beta, res_gamma
+        return res_raw, res_delta, res_theta, res_alpha, res_beta, res_gamma
 
     def _process_signal_type_participant(self, user: int, signal_type: str):
         eeg_person = self.load_participant(user)
