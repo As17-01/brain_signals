e4d370b7e9d6c573baab50a06a1437edf9dfb8d7
As17-01 <aseliverstov@nes.ru>
2025-07-21 00:52:56 +0300

    Fix liquid

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,4 @@ from src.models.GRULag1 import GRULag1
 from src.models.LSTMDefault import LSTMDefault
 from src.models.LSTMLag1 import LSTMLag1
 from src.models.LiquidDefault import LiquidDefault
+from src.models.LiquidLag1 import LiquidLag1
